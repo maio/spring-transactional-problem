13c9bc919e78167607d3816250c6ed59c47429bd
Marian Schubert <marian.schubert@gmail.com>
2022-07-29 15:28:14 +0200

    Reproduce the problem

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,48 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+class MyBoolean(var value: Boolean) {
+	fun get(): Boolean = value
+}
+
+abstract class Foo {
+	private val _hasError = MyBoolean(false)
+	val hasError: Boolean get() = _hasError.get()
+}
+
+@Component
+class MyFoo : Foo() {
+	@Transactional
+	fun whatever() {
+	}
+}
+
+@Component
+class MyBar : Foo() {
+	fun whatever() {
+	}
+}
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	lateinit var foo: MyFoo
+
+	@Autowired
+	lateinit var bar: MyBar
 
 	@Test
-	fun contextLoads() {
+	fun fooWorks() {
+		foo.hasError
 	}
 
+	@Test
+	fun barWorks() {
+		bar.hasError
+	}
 }
